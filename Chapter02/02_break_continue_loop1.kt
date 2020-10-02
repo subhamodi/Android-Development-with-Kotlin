f7358764f84392772463a1113d3ebb814cc8150a
subhamodi <72239664+subhamodi@users.noreply.github.com>
2020-10-02 08:36:26 +0530

    Update 02_break_continue_loop1.kt
    Spacingg

--- Chapter02/02_break_continue_loop1.kt
+++ Chapter02/02_break_continue_loop1.kt
@@ -1,15 +1,19 @@
-fun main(args: Array<String>) {
+fun main(args: Array<String>) 
+
+{
     val intRange = 1..6 
     val charRange = 'A'..'B' 
  
-    for(value in intRange) { 
+    for(value in intRange)
+    { 
         if(value == 3) 
             break 
  
-        println("Outer loop: $value ") 
+        println("Outer loop: $value") 
  
-        for (char in charRange) { 
-            println("\tInner loop: $char ") 
+        for (char in charRange) 
+        { 
+            println("\tInner loop: $char") 
         } 
     } 
 }
